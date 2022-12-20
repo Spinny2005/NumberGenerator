5c9021a0bc2c2cb2c82a73b7f6cb68fe21201004
Spencer Boggs <spencer_boggs@icloud.com>
2022-12-20 11:28:09 -0800

    Create numbergenerator.py

---
+++ numbergenerator.py
@@ -0,0 +1,13 @@
+import math
+while True:
+    result = float(input("Starting value: "))
+    step = float(input("Step size: "))
+    count = int(input("Steps: "))
+    decimal = int(input("Decimal places: "))
+    array = []
+    for x in range(count):
+        result = step + result
+        result = round(result, decimal)
+        array.append(result)
+    print(array)
+    
